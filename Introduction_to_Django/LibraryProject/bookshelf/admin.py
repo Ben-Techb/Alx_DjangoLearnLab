f4787bfd5d48056515963eb1cdcd671333299392
root <benbazirake9@gmail.com>
2025-02-13 23:53:24 -0800

    add the change to the file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import YourModel  # Replace 'YourModel' with your actual model name
 
-# Register your models here.
+admin.site.register(YourModel)  # Registers the model with the admin panel
+
