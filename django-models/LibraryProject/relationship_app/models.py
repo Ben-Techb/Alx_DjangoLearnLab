fed365f278e4bd41e8b78da3d93dd062074983ea
root <benbazirake9@gmail.com>
2025-02-18 03:24:50 -0800

    addd the change to the file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,7 +13,7 @@ class Library (models.Model):
     books = models.ManyToManyField(Book, related_name= 'libraries')
     def __str__(self):
         return self.name
-class Librarian (models.Model):
+class Librarian(models.Model):
     name= models.CharField(max_length= 200)
     library = models.OneToOneField(Library, on_delete= models.CASCADE, related_name='librarian')
     def __str__(self):
