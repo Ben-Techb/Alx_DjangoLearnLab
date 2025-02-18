97f430d2a7d837c82c9f9c7b3f9643d2b8d88797
root <benbazirake9@gmail.com>
2025-02-18 06:30:14 -0800

    add the file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+
 class Author(models.Model):
-    name= models.CharField(max_length = 200)
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
+
 class Book(models.Model):
-    title= models.CharField(max_length = 200)
-    authors= models.ForeignKey(Author, on_delete= models.CASCADE, related_name= 'Book')
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  # Fixed field name
+
+    def __str__(self):
+        return self.title  # Fixed incorrect attribute
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
     def __str__(self):
         return self.name
-class Library (models.Model):
-    name= models.CharField(max_length= 200)
-    books = models.ManyToManyField(Book, related_name= 'libraries')
-    def __str__(self):
-        return self.name
+
 class Librarian(models.Model):
-    name= models.CharField(max_length= 200)
-    library = models.OneToOneField(Library, on_delete= models.CASCADE, related_name='librarian')
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
     def __str__(self):
         return self.name
 
