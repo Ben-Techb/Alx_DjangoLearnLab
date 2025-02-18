773432fe939fec42a2276c6f61756161ea9dc9f0
root <benbazirake9@gmail.com>
2025-02-18 02:50:14 -0800

    add the change to the file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-class Author(models.model):
+class Author(models.Model):
     name= models.CharField(max_length = 200)
     def __str__(self):
         return self.name
-class Book(models.model):
+class Book(models.Model):
     title= models.CharField(max_length = 200)
     authors= models.ForeignKey(Author, on_delete= models.CASCADE, related_name= 'Book')
     def __str__(self):
