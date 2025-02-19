dca725dfee012f9ade794fbdcbbf5f985f4eecd1
root <benbazirake9@gmail.com>
2025-02-19 02:44:17 -0800

    add new data to url.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,11 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, register, LibraryDetailView
 
 urlpatterns = [
     path('book/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('relationship/', include('relationship_app.urls')),
-    path('register/', register, name='register'),
+    path('register/', register, name='register'),  
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
