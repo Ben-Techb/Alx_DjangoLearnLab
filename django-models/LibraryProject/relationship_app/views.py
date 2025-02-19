f6285858941e2f7df7c2a0ac412b53f6fcb5c3c7
root <benbazirake9@gmail.com>
2025-02-19 00:04:51 -0800

    add the change to the file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from . models import Book
-def book_list(request):
-    book= Book.object.all()
-    return render(request, 'book_list.html', {'books': books})
-from django.views.generic import DetailView, ListView
-from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Library, Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
