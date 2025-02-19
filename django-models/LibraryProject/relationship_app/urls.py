0cea08ad024a45dc92db1f732727fdd0df49c1be
root <benbazirake9@gmail.com>
2025-02-19 02:38:02 -0800

    add new data to url.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path,include
-from .views import list_books
-from . import views
+from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
-urlspatterns = [
-        path= ('book'/ , views. boook_list, name = 'book_list'),
-         path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-         path('relationship', include('relationship_app.urls')),
-         path('register'/, views.register, name = 'register'),
-         path('login'/,LoginViews.as_views(template_name='relationship_app/login.html') name= 'login'),
-         path('logout'/,LogoutViews.as_views(template_name='relationship_app/logout.html') name=' logout'),
-        ]
+from .views import list_books, register, LibraryDetailView
+
+urlpatterns = [
+    path('book/', list_books, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('relationship/', include('relationship_app.urls')),
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
+
