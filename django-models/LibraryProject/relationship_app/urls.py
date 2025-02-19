ce84eec3daa89370bc9061aee5eb0729390b8b8f
root <benbazirake9@gmail.com>
2025-02-19 02:23:45 -0800

    add the new data to urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,4 +6,7 @@ urlspatterns = [
         path= ('book'/ , views. boook_list, name = 'book_list'),
          path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
          path('relationship', include('relationship_app.urls')),
+         path('register'/, views.register, name = 'register'),
+         path('login'/, auth.views.LoginViews.as_views() name= 'login'),
+         path('logout'/,views.user_logout name=' logout'),
         ]
