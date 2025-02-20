bf7836cf3411b2404151329f80fb6b7548f59658
root <benbazirake9@gmail.com>
2025-02-20 05:49:02 -0800

    add the change to the file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, register, LibraryDetailView
+from relationship_app.views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('book/', list_books, name='book_list'),
@@ -8,5 +9,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
 
