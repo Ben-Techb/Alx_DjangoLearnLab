4338806b9a6fc311a82f5bd0c06f35a1ddaf29ae
Ben-Techb <benbazirake9@gmail.com>
2025-02-21 08:36:32 +0000

    add the new data

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('book/add/', views.add_book, name='add_book'),
     path('book/edit/<int:book_id>/', views.edit_book, name='edit_book'),
     path('book/delete/<int:book_id>/', views.delete_book, name='delete_book'),
-]
 
 
 
