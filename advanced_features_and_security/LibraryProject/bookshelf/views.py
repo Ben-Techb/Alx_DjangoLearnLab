aafb1024a3c56ac1951bfb742adfd685b545355c
root <benbazirake9@gmail.com>
2025-02-27 03:36:06 -0800

    add the change to the file to views file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+def raise_exception(request):
+    raise ValueError("Something went wrong!")
+
