08fbb9b9cbd55aee373413d816f6ca4c888bc568
root <benbazirake9@gmail.com>
2025-02-27 02:58:08 -0800

    add change to the admin file

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -14,4 +14,26 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register the Book model with the custom admin
 admin.site.register(Book, BookAdmin)
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
