53d41ad1cc52302701d98c4db4f5e91d9805f191
root <benbazirake9@gmail.com>
2025-02-27 03:53:31 -0800

    add change to forms.py file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,13 @@
-<form method="POST">
-    {% csrf_token %}
-    <input type="submit" value="Submit">
-</form>
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    author = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description']
 
