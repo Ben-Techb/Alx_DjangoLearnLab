9a51aa8b52773daf12d5a4f779f929d3555d7796
root <benbazirake9@gmail.com>
2025-03-11 04:10:26 -0700

    add the change to the view file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
-    path('api/', include('api.urls')),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,10 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
