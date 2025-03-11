df54c6f8234c7b26d76b8716519b6404ca81fab3
root <benbazirake9@gmail.com>
2025-03-11 03:42:34 -0700

    rename the project

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,13 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'rest_framework.authtoken',
-    'rest_framework.permissions.IsAuthenticated',
 ]
-REST_FRAMEWORK = {
+    REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ],
-    ...
+    ]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
